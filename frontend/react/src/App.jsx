9f8329888a56932f8a52f49365f27e4ef1c54788
Chima-Anason <tblynxx@gmail.com>
2024-02-18 04:53:40 +0800

    Intro to component, props, useState, useEffect

--- frontend/react/src/App.jsx
+++ frontend/react/src/App.jsx
@@ -1,5 +1,65 @@
+import UserProfile from "./UserProfile.jsx";
+import {useState, useEffect} from "react";
+
+const users = [
+    {
+        name: "Jamilia",
+        age: 28,
+        gender: "FEMALE"
+    },
+    {
+        name: "Ana",
+        age: 23,
+        gender: "FEMALE"
+    },
+    {
+        name: "Mike",
+        age: 30,
+        gender: "MALE"
+    }
+]
+
+const UserProfiles = ({users}) => (
+    <div>
+        {users.map((user,index)=>(
+            <UserProfile
+                key={index}
+                name={user.name}
+                age={user.age}
+                gender={user.gender}
+                imageNumber={index}
+            />
+        ))}
+    </div>
+)
 function App() {
-    return <h1>Hello React</h1>
+
+    const [counter, setCounter] = useState(0);
+    const [isLoading, setIsLoading] = useState(false);
+
+    useEffect(()=>{
+        setIsLoading(true)
+        setTimeout(()=>{
+            setIsLoading(false)
+        }, 4000)
+        return () => {
+            console.log("cleanup functions")
+        }
+    }, [])
+
+    if(isLoading){
+        return "loading..."
+    }
+    return (
+        <div>
+            <button
+                onClick={()=> setCounter(prevCounter => prevCounter+1)}>
+                Increment counter
+            </button>
+            <h1>{counter}</h1>
+            <UserProfiles users={users}/>
+        </div>
+    )
 }
 
 export default App
