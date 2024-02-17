89229e5d1acfa664f0a7b2b07a9eb8f3f1ddc9c6
Chima-Anason <tblynxx@gmail.com>
2024-02-18 07:08:01 +0800

    Setup and Customize SideBar

--- frontend/react/src/App.jsx
+++ frontend/react/src/App.jsx
@@ -1,8 +1,11 @@
 import {Button} from "@chakra-ui/react";
+import SidebarWithHeader from "./shared/SideBar.jsx";
 
 const App = () => {
     return (
-        <Button colorScheme='teal' variant='outline'>Click me</Button>
+        <SidebarWithHeader>
+            <Button colorScheme='teal' variant='outline'>Click me</Button>
+        </SidebarWithHeader>
     )
 }
 
