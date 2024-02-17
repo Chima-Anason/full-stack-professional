551d8f381b7d751d1db98af88a900f39f7dff180
Chima-Anason <tblynxx@gmail.com>
2024-02-18 07:50:16 +0800

    install Axios aand make request to getCustomer api from backend

--- frontend/react/src/App.jsx
+++ frontend/react/src/App.jsx
@@ -1,7 +1,17 @@
 import {Button} from "@chakra-ui/react";
 import SidebarWithHeader from "./shared/SideBar.jsx";
+import {useEffect} from "react";
+import {getCustomers} from "./services/client.js";
 
 const App = () => {
+
+    useEffect(()=>{
+        getCustomers().then(res => {
+            console.log(res)
+        }).catch(err => {
+            console.log(err)
+        })
+    })
     return (
         <SidebarWithHeader>
             <Button colorScheme='teal' variant='outline'>Click me</Button>
